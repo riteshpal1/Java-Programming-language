51be37a8d536ba5e3a490ba93bf587f1ffa49079
Ritesh Pal <8052ritesh@gmail.com>
2024-08-09 22:58:07 +0530

    123

---
+++ exercise.java
@@ -0,0 +1,38 @@
+import java.util.*;
+    public class exercise {
+        public static void main(String args[]){
+            Scanner sc = new Scanner(System.in);
+/*            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            int average = ( a + b + c)/3;
+        
+            System.out.println("avrage is: " + average);
+
+
+
+
+            int side = sc.nextInt();
+            int area = side*side;
+            System.out.println("area of sq: " + area );
+
+
+//  Enter cost of 3 items from the user (using float data type)- a pencil, a pen and
+// an eraser. You have to output the total cost of the items back to the user as their bill.
+// (Add on : You can also try adding 18% gst tax to the items in the bill as an advanced problem
+
+            float pen = (sc.nextFloat());
+            float pencil = (sc.nextFloat());
+            float eraser = (sc.nextFloat());
+
+            float total = pen + pencil + eraser;
+            System.out.println("total cost of items: " + total );
+*/
+
+
+
+
+    }
+    
+}
---
+++ operators.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+    public class operators {
+        /**
+         * @param args
+         */
+        public static void main(String[] args) {
+// Types of Operators
+//Arithmetic operators (Binary/Uniry) 
+//Relational operators
+//Bitwisee Operators
+//Assignment Operators
+
+//Arithmetic Operator(Binary(+,-,*,/,%),(++, --) )
+/*            int A= 10;
+            int B = 5;
+            System.out.println("add = " + (A+B));
+            System.out.println("sub = " + (A-B));
+            System.out.println("div = " + (A/B));
+            System.out.println("mul = " + (A*B));
+            System.out.println("reminder = " + (A%B));
+*/
+//Arithmetic Operator(Unary(++Increment,--Decrement)())
+
+  /*         // int A = 20;
+          //  int B = ++A;
+            // System.out.println(A);
+            
+            //int B  = A++;
+            //System.out.println(A);
+            
+           // int B = ++A;
+           // System.out.println(A);
+
+            int a = 10;
+            int b = a--;
+            System.out.println(a);
+            System.out.println(b);
+
+*/
+// Relational operrator
+/*
+ * ==
+ * !=
+ * <
+ * >
+ * >=
+ * <=
+ */
+ 
+// Logical operator
+/*
+ * &&(And),||(OR), !(Not)
+ */
+
+// Assignment oprator
+/* =,+=,-=,*=,/= */
+
+        }
+}
